83c19958fc20ff5b4d048bf0b619b78ec4199827
Svene <hello.svene@gmail.com>
2024-06-06 21:57:52 +0800

    percentage adjustments

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -6,5 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getPercentage(value: number): number {
-  return (1 - Math.min(Math.max(value, 13), 40) / 40) * 100;
+  return (1 - Math.min(Math.max(value, 10), 40) / 40) * 100;
 }
